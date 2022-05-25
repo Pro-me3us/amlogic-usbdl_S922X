18a1303718aa145d823f96bb5581ead7fbc566b6
Pro-me3us <106105250+Pro-me3us@users.noreply.github.com>
2022-05-25 13:04:06 -0400

    Update bl2_payload.c

--- payloads/bl2_payload.c
+++ payloads/bl2_payload.c
@@ -1,3 +1,6 @@
+/*
+ * mini payload to be merged with Bl2 payload, jumps 0x1000 header and clears USB state 
+ */
 #define _clear_icache() ((void (*)(void))0xffff0534)()   //s905d3 - 0xffff048c
 #define _dwc_pcd_irq() ((void (*)(void))0xffff6d7c)()    //s905d3 - 0xffff8250
 #define _jmp_bl2() ((void (*)(void))0xfffa1000)()
