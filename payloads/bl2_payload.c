5adac21393e6857bc25197da9943d353d94b609a
Pro-me3us <106105250+Pro-me3us@users.noreply.github.com>
2022-05-25 12:48:35 -0400

    Add files via upload

---
+++ payloads/bl2_payload.c
@@ -0,0 +1,10 @@
+#define _clear_icache() ((void (*)(void))0xffff0534)()   //s905d3 - 0xffff048c
+#define _dwc_pcd_irq() ((void (*)(void))0xffff6d7c)()    //s905d3 - 0xffff8250
+#define _jmp_bl2() ((void (*)(void))0xfffa1000)()
+void _start()
+{
+    _clear_icache(); //clear instruction cache
+    _dwc_pcd_irq();  //clear USB state
+    _dwc_pcd_irq();  //after exploitation
+    _jmp_bl2();      //jump to BL2 entrypoint
+}
